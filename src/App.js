cace1db0d0b24915644e10a008cc259afb3888b0
Mrtgk93 <mrtgk93@hotmail.com>
2023-02-14 21:26:35 +0300

    tasarımı düzeltmeye çalıştım

--- src/App.js
+++ src/App.js
@@ -7,6 +7,85 @@ const Div = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
+  width: 100vw;
+  background-color: black;
+  color: white;
+`;
+const FDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+const ScButton = styled.button`
+  background-color: azure;
+  border-radius: 16px;
+  width: 100px;
+  height: 30px;
+  margin-left: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: lightblue;
+  }
+`;
+
+const FooterButton = styled.button`
+  margin-top: 5vh;
+  width: 30vw;
+  height: 50px;
+  background-color: white;
+  border-radius: 20px;
+  cursor: pointer;
+  &:hover {
+    background-color: lightblue;
+  }
+`;
+const ScFooter = styled.footer`
+  background-color: black;
+  height: 15vh;
+`;
+const divStyle = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+};
+const FP1 = styled.p`
+  font-size: 20px;
+  margin: 0;
+`;
+
+const Sch1 = styled.h1`
+  width: 500px;
+`;
+const DivCard = styled.div`
+  height: 40vh;
+  width: 21vw;
+  background-color: white;
+  color: black;
+  border-radius: 30px;
+  margin-top: 2vh;
+`;
+const A = styled.a`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
+const CImg = styled.img`
+  box-sizing: border-box;
+  width: 21vw;
+  height: 20vh;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`;
+const CardButton = styled.button`
+  width: 160px;
+  height: 30px;
+  border-radius: 20px;
+  &:hover {
+    background-color: #505061;
+    color: white;
+  }
+  cursor: pointer;
 `;
 
 const App = () => {
@@ -29,23 +108,175 @@ const App = () => {
   return (
     <Div>
       <BrowserRouter>
-        <h1>Teknolojik Yemekler</h1>
-        <div>
-          <Link to="/">
-            <button id="order-pizza">Ana Sayfa</button>
-          </Link>
-          <Link to="/pizza">
-            <button>Sipariş Formu</button>
-          </Link>
-        </div>
+        <div style={divStyle}>
+          <img
+            src="	https://cdn-icons-png.flaticon.com/256/5965/5965340.png"
+            height="70px"
+            width="70px"
+          />
+          <Sch1>Teknolojik Yemekler</Sch1>
+          <div>
+            <Link to="/">
+              <ScButton id="order-pizza">Ana Sayfa</ScButton>
+            </Link>
 
+            <Link to="/pizza">
+              <ScButton>Sipariş Formu</ScButton>
+            </Link>
+          </div>
+        </div>
         <Switch>
-          <Route exact path="/"></Route>
+          <Route exact path="/">
+            <main>
+              <div>
+                <A>
+                  <DivCard>
+                    <div>
+                      <CImg src="	https://cdn.dpeurasia.com/dms/images/product/TR__T611_434x404.jpg" />
+                    </div>
+                    <br />
+                    Orta Boy Pizzalar
+                    <div>
+                      <br />
+                      124 TL <br />
+                      <br />
+                      "den başlayan fiyatlarla
+                      <br />
+                      <br />
+                      <CardButton>Sipariş Ver</CardButton>
+                    </div>
+                  </DivCard>
+                  <DivCard>
+                    <div>
+                      <CImg src="https://cdn.dpeurasia.com/dms/images/product/TR__T0425_434x404.jpg" />
+                    </div>
+                    <br />
+                    Büyük Boy Bol Malzemos
+                    <div>
+                      <br />
+                      129 TL <br />
+                      <br />
+                      "den başlayan fiyatlarla
+                      <br />
+                      <br />
+                      <CardButton>Sipariş Ver</CardButton>
+                    </div>
+                  </DivCard>
+                  <DivCard>
+                    <div>
+                      <CImg src="https://cdn.dpeurasia.com/dms/images/product/TR__T0442_434x404.jpg" />
+                    </div>
+                    <br />2 Adet Pizetta
+                    <div>
+                      <br />
+                      59 TL <br />
+                      <br />
+                      "den başlayan fiyatlarla
+                      <br />
+                      <br />
+                      <CardButton>Sipariş Ver</CardButton>
+                    </div>
+                  </DivCard>
+                  <DivCard>
+                    <div>
+                      <CImg src="https://cdn.dpeurasia.com/dms/images/product/TR__T0353_434x404.jpg" />
+                    </div>
+                    <br />2 Orta Boy Pizza
+                    <div>
+                      <br />
+                      150 TL <br />
+                      <br />
+                      "den başlayan fiyatlarla
+                      <br />
+                      <br />
+                      <CardButton>Sipariş Ver</CardButton>
+                    </div>
+                  </DivCard>
+                  <DivCard>
+                    <div>
+                      <CImg src="https://cdn.dpeurasia.com/dms/images/product/TR__T0475_434x404.jpg" />
+                    </div>
+                    <br />3 Orta Boy Bol Malzemos
+                    <div>
+                      <br />
+                      240 TL <br />
+                      <br />
+                      "den başlayan fiyatlarla
+                      <br />
+                      <br />
+                      <CardButton>Sipariş Ver</CardButton>
+                    </div>
+                  </DivCard>
+                  <DivCard>
+                    <div>
+                      <CImg src="https://cdn.dpeurasia.com/dms/images/product/TR__T0406_434x404.jpg" />
+                    </div>
+                    <br />
+                    Coca Cola Mucize Menü
+                    <div>
+                      <br />
+                      269 TL <br />
+                      <br />
+                      "den başlayan fiyatlarla
+                      <br />
+                      <br />
+                      <CardButton>Sipariş Ver</CardButton>
+                    </div>
+                  </DivCard>
+                  <DivCard>
+                    <div>
+                      <CImg src="https://cdn.dpeurasia.com/dms/images/product/TR__T0446_434x404.jpg" />
+                    </div>
+                    <br />
+                    Pizetta + Bol <br /> Peynirli Ekmek
+                    <div>
+                      74 TL <br />
+                      <br />
+                      "den başlayan fiyatlarla
+                      <br />
+                      <br />
+                      <CardButton>Sipariş Ver</CardButton>
+                    </div>
+                  </DivCard>
+                  <DivCard>
+                    <div>
+                      <CImg src="	https://cdn.dpeurasia.com/dms/images/product/TR__T882_434x404.jpg" />
+                    </div>
+                    <br />
+                    2 Adet Çikolatalı <br /> Sufle
+                    <div>
+                      74 TL <br />
+                      <br />
+                      "den başlayan fiyatlarla
+                      <br />
+                      <br />
+                      <CardButton>Sipariş Ver</CardButton>
+                    </div>
+                  </DivCard>
+                </A>
+              </div>
+            </main>
+          </Route>
+
           <Route exact path="/pizza">
             <Form handleSubmitCallBack={siparisEkle} />
           </Route>
         </Switch>
       </BrowserRouter>
+
+      <ScFooter>
+        <FDiv>
+          <FooterButton>
+            <FP1>Promosyon Kodu</FP1>
+          </FooterButton>
+          <FooterButton>
+            <FP1>Pizza Takip</FP1>
+          </FooterButton>
+          <FooterButton>
+            <FP1>Kampanyalar</FP1>
+          </FooterButton>
+        </FDiv>
+      </ScFooter>
     </Div>
   );
 };
--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,45 @@
 import { useState } from "react";
 import * as Yup from "yup";
+import styled from "styled-components";
+import { Row } from "reactstrap";
+
+const SiparisButton = styled.button`
+  width: 40vw;
+  height: 40px;
+  background-color: #14b351;
+  color: white;
+  cursor: pointer;
+  border-radius: 50px;
+  font-size: 18px;
+`;
+const ıconImg = styled.img`
+  margin-right: 5px;
+`;
+const inputStyle = {
+  width: "15vw",
+};
+const fieldStyle = {
+  width: "50vw",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+const formStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+const labelStyle = {
+  fontSize: "18px",
+};
 
 const formSchema = Yup.object().shape({
   isim: Yup.string()
     .required("İsim alanı zorunludur")
     .min(2, "İsim en az 2 karakter olmalıdır"),
   boyut: Yup.string().required("lütfen seçiniz"),
+  kenar: Yup.string().required("lütfen seçiniz"),
+  hamur: Yup.string().required("lütfen seçiniz"),
   malzeme1: Yup.bool().oneOf([false]),
   malzeme2: Yup.bool().oneOf([false]),
   malzeme3: Yup.bool().oneOf([false]),
@@ -18,6 +52,8 @@ export default function Form(props) {
   const [data, setData] = useState({
     isim: "",
     boyut: "",
+    kenar: "",
+    hamur: "",
     malzeme1: false,
     malzeme2: false,
     malzeme3: false,
@@ -28,6 +64,8 @@ export default function Form(props) {
   const [errors, setErrors] = useState({
     isim: "",
     boyut: "",
+    kenar: "",
+    hamur: "",
     malzeme1: false,
     malzeme2: false,
     malzeme3: false,
@@ -71,13 +109,14 @@ export default function Form(props) {
 
   return (
     <div>
-      <form id="pizza-form" onSubmit={handleSubmit}>
-        <fieldset>
+      <form style={formStyle} id="pizza-form" onSubmit={handleSubmit}>
+        <fieldset style={fieldStyle}>
           <legend>Siparişinizi Oluşturun</legend>
           <p>
             <label>
               İsim:
               <input
+                style={inputStyle}
                 onChange={changeHandlerName}
                 type="text"
                 name="isim"
@@ -88,6 +127,12 @@ export default function Form(props) {
           </p>
           {errors.isim !== "" && <div>{errors.isim}</div>}
           <p>
+            <ıconImg
+              className="boyut-icon"
+              src="	https://cdn.dpeurasia.com/dms/images/icon/sizes.png"
+              alt="sizes"
+              width="20"
+            />
             <label>
               Pizza Boyutu:
               <select name="boyut" id="dropdown" onChange={changeHandlerName}>
@@ -101,38 +146,76 @@ export default function Form(props) {
             </label>
           </p>
           <p>
-            <label>Sosis</label>
+            <ıconImg
+              className="kenar-icon"
+              src="https://cdn.dpeurasia.com/dms/images/icon/sides.png"
+              alt="sizes"
+              width="20"
+            />
+            <label>
+              Kenar Tipi:
+              <select name="kenar" id="kenar" onChange={changeHandlerName}>
+                <option value={data.value}>Klasik Kenar</option>
+                <option name="max" value={data.value}>
+                  Sarımsak Kenar
+                </option>
+                <option value={data.value}>Sucuk Kenar</option>
+              </select>
+            </label>
+          </p>
+          <p>
+            <ıconImg
+              className="hamur-icon"
+              src="	https://cdn.dpeurasia.com/dms/images/icon/doughs.png"
+              alt="sizes"
+              width="20"
+            />
+            <label>
+              Hamur Tipi:
+              <select name="hamur" id="hamur" onChange={changeHandlerName}>
+                <option value={data.value}>Klasik Hamur</option>
+                <option name="max" value={data.value}>
+                  İnce Hamur
+                </option>
+              </select>
+            </label>
+          </p>
+          <p>
             <input
               onChange={changeHandlerName}
               type="checkbox"
               name="malzeme1"
             />
+            <label>Sosis</label>
           </p>
           {errors.özel !== "" && <div>{errors.özel}</div>}
           <p>
-            <label>Sucuk</label>
             <input
               onChange={changeHandlerName}
               type="checkbox"
               name="malzeme2"
+              data-cy="datasucuk"
             />
+            <label>Sucuk</label>
           </p>
           <p>
-            <label>Mantar</label>
             <input
               onChange={changeHandlerName}
               type="checkbox"
               name="malzeme3"
+              data-cy="datamantar"
             />
+            <label>Mantar</label>
           </p>
           <p>
-            <label>Biber</label>
             <input
               onChange={changeHandlerName}
               type="checkbox"
               name="malzeme4"
             />
+            <label>Biber</label>
           </p>
+
           <p>
             <label>Özel Seçim:</label>
             <input
@@ -142,9 +225,9 @@ export default function Form(props) {
               name="özel"
             />
           </p>
-          <button type="submit" data-cy="datasubmit" id="order-button">
+          <SiparisButton type="submit" data-cy="datasubmit" id="order-button">
             Siparişlere Ekle
-          </button>
+          </SiparisButton>
         </fieldset>
       </form>
     </div>
